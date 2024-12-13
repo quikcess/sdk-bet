10624174fb1a2e378f1eec207258b75f63766c7c
Eydrenn <endreyestudos67@gmail.com>
2024-12-12 21:51:26 -0300

    refactor: updates

--- test/bet.test.ts
+++ test/bet.test.ts
@@ -1,6 +1,8 @@
 import { bettingApi } from "./index.test";
 
+console.time("bet-get")
 bettingApi.bets.getById("098").then(console.log);
+console.timeEnd("bet-get")
 
 bettingApi.on("betCreate", async (data) => {
 	console.log(`Veja Mais: ${data.betId}`, data);
