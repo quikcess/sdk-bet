cce1368ce543cfc1a8a732311f3cce9ef8dbde32
Eydrenn <endreyestudos67@gmail.com>
2024-12-15 20:41:29 -0300

    refactor: updates

--- src/types/cases.ts
+++
@@ -1,7 +0,0 @@
-type SnakeToCamelCase<S extends string> = S extends `${infer T}_${infer U}`
-  ? `${T}${Capitalize<SnakeToCamelCase<U>>}`
-  : S;
-
-type CamelCase<T> = {
-  [K in keyof T as SnakeToCamelCase<string & K>]: T[K];
-};
