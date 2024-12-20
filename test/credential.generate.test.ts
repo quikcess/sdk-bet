2abece22c87d5c53a778488c08ebbd3c321dfbf5
Eydrenn <endreyestudos67@gmail.com>
2024-12-20 12:11:12 -0300

    refactor: updates

--- test/credential.generate.test.ts
+++ test/credential.generate.test.ts
@@ -1,5 +1,5 @@
 import { bettingApi } from "./index.test";
 
 console.time("credentials");
-bettingApi.credentials.generate("123456", "wagner", "silver").then(console.log);
+bettingApi.credentials.generate("123456", "wagner", 3).then(console.log);
 console.timeEnd("credentials");
